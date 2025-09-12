c81e2de351e63feae42666303d1fb12398ecd675
crissiiu <crissiiu.dev@gmail.com>
2025-09-12 21:40:32 +0700

    Edit package json, setup debug nestjs,  add validator for user schema
    Edit package.json
    - Update mapped-types to version 2.1.0
    - Add class-transformers and class-validator
    
    Setup debug nestjs [launch.json file]
    
    Add validator
    - Add Validation Pipe
    - Setup validator, setup miss field
    - Test with users schema

--- FullStack_Next_Nest_Practice/02-nest-rn/src/modules/users/dto/create-user.dto.ts
+++ FullStack_Next_Nest_Practice/02-nest-rn/src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserDto {}
+import { IsEmpty, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmpty({ message: 'name không được để trống' })
+  name: string;
+
+  email: string;
+  password: string;
+  phone: string;
+  address: string;
+  image: string;
+}
