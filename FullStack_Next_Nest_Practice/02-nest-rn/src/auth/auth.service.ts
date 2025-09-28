df17a50ae2b82f8230cf4779dd22bfa18e7be676
crissiiu <crissiiu.dev@gmail.com>
2025-09-28 22:41:09 +0700

    Done create access token

--- FullStack_Next_Nest_Practice/02-nest-rn/src/auth/auth.service.ts
+++ FullStack_Next_Nest_Practice/02-nest-rn/src/auth/auth.service.ts
@@ -1,18 +1,28 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '@/modules/users/users.service';
 import { comparePasswordHelper } from '@/helpers/util';
+import { access } from 'fs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async SignIn(username: string, pwd: string): Promise<any> {
     const user = await this.usersService.findByEmail(username);
-    if (!comparePasswordHelper(pwd, user.password)) {
-      throw new UnauthorizedException();
+    const isValidPassword = await comparePasswordHelper(pwd, user.password);
+
+    if (!isValidPassword) {
+      throw new UnauthorizedException('Username or password is not valid!');
     }
 
-    const { password, ...result } = user;
-    return result;
+    const payload = { sub: user._id, username: user.email };
+
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
