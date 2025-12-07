794414abd9cc1811e89e617f2c0ee885fa53ae7a
crissiiu <crissiiu.dev@gmail.com>
2025-12-07 16:05:24 +0700

    Created a console app with C# Dotnet 8.0

---
+++ DotNet/1-Basic-Console-App/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            if (args.Length > 0)
+            {
+                foreach (var arg in args)
+                {
+                    Console.WriteLine($"Argument: {arg}");
+                }
+            }
+
+            var s = Console.ReadLine();
+            if (int.TryParse(s, out var val))
+            {
+                Console.WriteLine($"You entered the number: {val}");
+            }
+            else
+            {
+                Console.WriteLine("You did not enter a valid number.");
+            }
+            Console.WriteLine(val);
+        }
+    }
+}
