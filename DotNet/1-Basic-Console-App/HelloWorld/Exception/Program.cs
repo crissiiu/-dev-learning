b4ea49f97d58a572b0204a269fe522bbd895a035
crissiiu <crissiiu.dev@gmail.com>
2025-12-14 22:12:41 +0700

    Exception

---
+++ DotNet/1-Basic-Console-App/HelloWorld/Exception/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+namespace Exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            try
+            {
+                int n = 0;
+                int x = 10 / n;
+                Console.WriteLine(x);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Lỗi: " + ex.Message);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Lỗi định dạng: " + ex.Message);
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
