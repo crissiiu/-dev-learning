f5f99d85139e8ba9277ef13309565758586bfadf
crissiiu <crissiiu.dev@gmail.com>
2025-12-14 23:19:07 +0700

    stream and file

---
+++ DotNet/1-Basic-Console-App/HelloWorld/copy/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace copy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"D:\\Documents\\Nguyen-Thanh-Sieu.txt";
+            string destinationPath = @"D:\\Documents\\Nguyen-Thanh-Sieu-Copy.txt";
+
+            var buffer = new byte[1024];
+            using var instream = File.OpenRead(sourcePath);
+            //using var outstream = File.OpenWrite(sourcePath);
+
+            int n = instream.Read(buffer, 0, buffer.Length);
+            while (n > 0)
+            {
+                Console.WriteLine(n.ToString());
+                //outstream.Write(buffer, 0, n);
+                n = instream.Read(buffer, 0, buffer.Length);
+            }
+            //instream.Close();
+            //outstream.Close();
+        }
+    }
+}
